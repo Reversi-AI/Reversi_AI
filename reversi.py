70ffa8f6e9cb5bcd40f1d7346ebd4caa1813c63f
bbrianh <brianhomanchon@gmail.com>
2021-03-27 01:23:29 +0800

    Update reversi.py

--- reversi.py
+++ reversi.py
@@ -26,14 +26,19 @@ from typing import Optional
 import plotly.graph_objects as go
 from plotly.subplots import make_subplots
 
-
 ################################################################################
 # Representing Reversi
 ################################################################################
 # representation of occupancy of a square on the board
-_EMPTY = ''
+_EMPTY = '_'
 _BLACK = 'X'
 _WHITE = 'O'
+
+# mapping used for converting move between algebraic and index
+_FILE_TO_INDEX = {'a': 0, 'b': 1, 'c': 2, 'd': 3, 'e': 4, 'f': 5, 'g': 6, 'h': 7}
+_INDEX_TO_FILE = {i: f for f, i in _FILE_TO_INDEX.items()}
+_RANK_TO_INDEX = {'1': 0, '2': 1, '3': 2, '4': 3, '5': 4, '6': 5, '7': 6, '8': 7}
+_INDEX_TO_RANK = {i: r for r, i in _RANK_TO_INDEX.items()}
 
 
 class ReversiGame:
@@ -47,27 +52,27 @@ class ReversiGame:
     #   - _num_white_pieces: an integer representing the number of white pieces on the board
 
     # Representation Invariants:
-    #   - len(_board) == 8
-    #   - all(len(row) == 8 for row in _board)
-    #   - all elements in the inner lists of _board is in {_EMPTY, _BLACK, _WHITE}
+    #   - len(self_board) == 8
+    #   - all(len(row) == 8 for row in self._board)
+    #   - all elements in the inner lists of self._board is in {_EMPTY, _BLACK, _WHITE}
+    #   - self._turn in {_BLACK, _WHITE}
     _board: list[list[str]]
     _valid_moves: list[str]
-    _is_black_turn: bool
+    _turn: str
 
-    def __init__(self, board: list[list[Optional[str]]] = None,
-                 black_turn: bool = True) -> None:
+    def __init__(self, board: list[list[Optional[str]]] = None, turn: str = _BLACK) -> None:
         """Initialize a game with the given state. The board is empty if board is None.
 
         Representation Invariants:
             - board is None or len(_board) == 8
             - board is None or all(len(row) == 8 for row in _board)
-            - board is None or every element in the inner lists of _board is in {_EMPTY,
-                                                                                 _BLACK, _WHITE}
+            - board is None or every element in the inner lists of _board is in
+            {_EMPTY, _BLACK, _WHITE}
+            - turn in {_BLACK, _WHITE}
 
         :param board: the given board state of the game. None by default when initializing a new
         game
-        :param black_turn: boolean representing whether black is the current active player. True
-        by default when initializing a new game
+        :param turn: str representing which player should go next
         :return: None
         """
         if board is None:
@@ -77,14 +82,28 @@ class ReversiGame:
                 self._board.append([_EMPTY] * 8)
 
             # place 2 black and 2 white pieces on the center
-            self._board[3][3], self._board[4][4] = _WHITE, _WHITE
-            self._board[3][4], self._board[4][3] = _BLACK, _BLACK
+            self._board[3][3], self._board[3][4] = _WHITE, _BLACK
+            self._board[4][3], self._board[4][4] = _BLACK, _WHITE
 
         else:
             self._board = board
 
-        self._is_black_turn = black_turn
-        self._valid_moves = self._calculate_valid_moves(self._board, self._is_black_turn)
+        self._turn = turn
+        self._update_valid_moves()
+
+    def print_game_board(self) -> None:
+        """Return a string representing the state of the board
+
+        :return: None
+        """
+        print('*' * 26)
+
+        print('   ' + '  '.join([c for c in 'abcdefgh']))
+
+        for i in range(len(self._board)):
+            print(f'{i + 1}  ' + '  '.join(self._board[i]))
+
+        print('*' * 26)
 
     def get_valid_moves(self) -> list[str]:
         """Return a list of the valid moves for the active player
@@ -93,48 +112,68 @@ class ReversiGame:
         """
         return self._valid_moves
 
+    def _next_player(self) -> None:
+        """Mutate self.turn to the next player"""
+        if self._turn == _BLACK:
+            self._turn = _WHITE
+        else:
+            self._turn = _BLACK
+
     def make_move(self, move: str) -> None:
         """Make the given move and mutate the instance attributes of self such that
         self would represent the updated game state after the move is made.
 
         If move is not a currently valid move, raise a ValueError.
 
-        :param move: the given move to be made
+        Preconditions:
+            - self.get_winner() is None
+
+        :param move: the move to be made
         :return: None
         """
-        # TODO
+        if move not in self._valid_moves:
+            raise ValueError(f'Move "{move}" is invalid')
+
+        self._update_board(self._turn, move)
+        self._next_player()
+        self._update_valid_moves()
 
     def simulate_move(self, move: str) -> ReversiGame:
         """Make the given move in a copy of self, and return the copy after the move is made.
 
         If move is not a currently valid move, raise a ValueError.
+
+        :param move: the move to be made
+        :return: a copy the the game state after the move is made
         """
-        # TODO
+        copy_state = copy.deepcopy(self)
+        copy_state.make_move(move)
+        return copy_state
 
-    def is_black_move(self) -> bool:
-        """Return whether the black player is to move next.
+    def get_current_player(self) -> str:
+        """Return which player is going to play next
 
-        :return: whether the black player is to move next
+        :return: a string representing which player will play next
         """
-        return self._is_black_turn
+        return self._turn
 
-    def get_num_pieces(self, piece: str) -> int:
-        """Return the number of the given piece on the current board.
+    def get_num_pieces(self) -> tuple[int, int]:
+        """Return the number of piece of each color on the board.
 
-        Preconditions:
-            - piece in {_BLACK, _WHITE}
-
-        :param piece: the piece to count on the current board
-        :return: the number of the given piece on the current board
+        :return: a tuple where the first element represents the number of black pieces
+        and the second element represents the number of white pieces
         """
-        num_piece_so_far = 0
+        black_so_far = 0
+        white_so_far = 0
 
         for row in self._board:
             for square in row:
-                if square == piece:
-                    num_piece_so_far += 1
+                if square == _BLACK:
+                    black_so_far += 1
+                if square == _WHITE:
+                    white_so_far += 1
 
-        return num_piece_so_far
+        return (black_so_far, white_so_far)
 
     def get_winner(self) -> Optional[str]:
         """Return the winner of the game (black or white) or 'draw' if the game ended in a draw.
@@ -144,23 +183,150 @@ class ReversiGame:
         :return: winner of the game (black or white) or 'draw' if the game ended in a draw.
         None if the game is not over.
         """
-        if len(self._valid_moves) == 0:
-            if len(self._calculate_valid_moves(self._board, not self._is_black_turn)) == 0:
-                num_black_pieces = self.get_num_pieces(_BLACK)
-                num_white_pieces = self.get_num_pieces(_WHITE)
-
-                if num_black_pieces > num_white_pieces:
-                    return 'black'
-                elif num_black_pieces == num_white_pieces:
-                    return 'draw'
-                else:
-                    return 'white'
+        if self._is_valid_move(_BLACK, 'pass') and self._is_valid_move(_WHITE, 'pass'):
+            black, white = self.get_num_pieces()
+            if black > white:
+                return 'black'
+            elif black == white:
+                return 'draw'
+            else:
+                return 'white'
         else:
             return None
 
-    def _calculate_valid_moves(self, board: list[list[str]], is_black_active: bool) -> list:
-        """Return all possible moves on a given board state with a given active player."""
-        # TODO
+    def _update_board(self, turn: str, move: str) -> None:
+        """Mutate self._board after the given player made the move. Note that move can be 'pass'
+
+        Preconditions:
+            - move is currently a legal move for the active player.
+
+        """
+        if move != 'pass':
+            y_move, x_move = _algebraic_to_index(move)
+            self._board[y_move][x_move] = turn
+
+            flips_so_far = []
+            directions = [(0, 1), (0, -1), (1, 0), (-1, 0),
+                          (1, 1), (-1, 1), (1, -1), (-1, -1)]
+            for direction in directions:
+                flips_so_far.extend(self._check_flips(turn, move, direction))
+            for y, x in flips_so_far:
+                self._board[y][x] = turn
+
+    def _update_valid_moves(self) -> None:
+        """Mutate self._valid_moves"""
+        self._valid_moves = []
+        for y in range(len(self._board)):
+            for x in range(len(self._board)):
+                move = _index_to_algebraic((y, x))
+                if self._is_valid_move(self._turn, move):
+                    self._valid_moves.append(move)
+
+        if len(self._valid_moves) == 0:  # no valid moves
+            self._valid_moves.append('pass')
+
+    def _is_valid_move(self, player: str, move: str) -> bool:
+        """Return whether the given move is valid for the given player.
+
+         Preconditions:
+            - player in {_BLACK, _WHITE}
+            - move is a coordinate on the board
+
+        :param player: the player making the move
+        :param move: the move being made
+        :return: whether the given move is valid for the given player
+        """
+        y, x = _algebraic_to_index(move)
+        if self._board[y][x] != _EMPTY:
+            return False
+
+        directions = [(0, 1), (0, -1), (1, 0), (-1, 0),
+                      (1, 1), (-1, 1), (1, -1), (-1, -1)]
+        for direction in directions:
+            if len(self._check_flips(player, move, direction)) != 0:
+                return True
+        return False
+
+    def _check_flips(self, player: str, move: str, direction: tuple) -> list[tuple[int, int]]:
+        """Assume the player plays a given move, check the given direction for which pieces
+        can be flipped.
+
+        direction[0] represents dy which is the proceeding direction of y.
+        direction[1] represents dx which is the proceeding direction of x.
+        For example, if player = _BLACK, move = 'c4', direction =(0, 1), the function would
+        return a list of positions of white pieces that can be flipped on the right of c4
+
+        Preconditions:
+            - The square of move is empty
+            - player in {_BLACK, _WHITE}
+            - direction[0] in {-1, 0, 1}
+            - direction[1] in {-1, 0, 1}
+
+        :param player: the player playing the move
+        :param move: the move begin played
+        :param direction: the direction to be checked for flips
+        """
+        y, x = _algebraic_to_index(move)
+        dy, dx = direction
+
+        if player == _BLACK:
+            opponent = _WHITE
+        else:
+            opponent = _BLACK
+
+        if not self._is_on_board((y + dy, x + dx)):
+            return []
+        if not self._is_on_board((y + dy + dy, x + dx + dx)):
+            return []
+        if self._board[y + dy][x + dx] != opponent:
+            return []
+
+        y += dy
+        x += dx
+        # self._board[y][x] now is opponent's color
+        flips_so_far = [(y, x)]
+        while self._is_on_board((y, x)) and self._board[y][x] == opponent:
+            y += dy
+            x += dx
+            flips_so_far.append((y, x))
+
+        if not self._is_on_board((y, x)):
+            return []
+        elif self._board[y][x] == player:
+            return flips_so_far
+        else:
+            return []
+
+    def _is_on_board(self, pos: tuple[int, int]) -> bool:
+        """Return whether coordinates in array indices pos is a valid position on the game board
+
+        :param pos: coordinates in array indices
+        """
+        return 0 <= pos[0] <= 7 and 0 <= pos[1] <= 7
+
+
+def _algebraic_to_index(move: str) -> tuple[int, int]:
+    """Convert coordinates in algebraic format ex. 'a2' to array indices (y, x).
+
+    Preconditions:
+        - move[0] in _FILE_TO_INDEX
+        - move[1] in _RANK_TO_INDEX
+
+    :param move: coordinates in algebraic format
+    """
+    return (_RANK_TO_INDEX[move[1]], _FILE_TO_INDEX[move[0]])
+
+
+def _index_to_algebraic(pos: tuple[int, int]) -> str:
+    """Convert coordinates in array indices (y, x) to algebraic format.
+
+    Preconditions:
+        - pos[0] in _FILE_TO_INDEX
+        - pos[1] in _RANK_TO_INDEX
+
+    :param pos: coordinates in array indices
+    """
+    return _INDEX_TO_FILE[pos[1]] + _INDEX_TO_RANK[pos[0]]
 
 
 ################################################################################
