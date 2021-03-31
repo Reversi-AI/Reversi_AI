8c2dff82173e308d6e81ea39d935b6b7634db365
TheXanMan321 <xander4444@icloud.com>
2021-03-31 10:22:07 -0400

    Revert "You can use the mouse to make moves"
    This reverts commit e35eb9d7b6c1f0712e8b7a8a399814a219a2aefc.

--- tk_gui.py
+++ tk_gui.py
@@ -1,14 +1,14 @@
 import tkinter as tk
 import time
 
-from reversi import ReversiGame, Player, RandomPlayer, GUIPlayer, _index_to_algebraic
+from reversi import ReversiGame, Player, RandomPlayer
 from constants import BLACK, WHITE
 
 
 
 class ReversiGUI:
     """"""
-    def __init__(self, parent, size) -> None:
+    def __init__(self, parent, size, player1, player2) -> None:
         """initialize gui"""
         # setting up
         self.root = parent
@@ -16,17 +16,15 @@ class ReversiGUI:
         self.frame = tk.Frame(parent)
         self.board = tk.Canvas(height=500, width=500, bg='black')
         self.frame.pack()
-        self.click_pos = (-1,-1)
 
         # initialize game
         self.game = ReversiGame(size)
-        # self.player1 = player1
-        # self.player2 = player2
+        self.player1 = player1
+        self.player2 = player2
         self.draw_game_state()
-        self.board.bind('<Button-1>', self.click)
 
         # run game
-        # self.run_game(self.player1, self.player2)
+        self.run_game(self.player1, self.player2)
 
     def run_game(self, black, white) -> None:
         """Run a Reversi game between the two given players.
@@ -48,35 +46,6 @@ class ReversiGUI:
                 current_player = white
             else:
                 current_player = black
-
-    def run_game_human(self, black, white) -> None:
-        """Run a Reversi game between the two given players.
-
-        Return the winner and list of moves made in the game.
-        """
-        previous_move = None
-        current_player = black
-        self.draw_game_state()
-
-        while self.game.get_winner() is None:
-
-            # previous_move = current_player.make_move(self.game, previous_move)
-            self.game.make_move(self.human_move())
-            self.draw_game_state()
-            time.sleep(1)
-            root.update()
-
-            if current_player is black:
-                current_player = white
-            else:
-                current_player = black
-
-
-    def human_move(self) -> str:
-        move = None
-        while move not in self.game.get_valid_moves():
-            root.wait_variable(self.click_pos)
-            return _index_to_algebraic(self.click_pos)
 
     def draw_game_state(self, h=500, w=500) -> None:
         """Visualize the game by drawing on the Canvas"""
@@ -104,29 +73,10 @@ class ReversiGUI:
 
         self.board.pack()
 
-    def click(self, event) -> None:
-        """Called when mouse is clicked on the given canvas"""
-        xcor = event.x // (self.board.winfo_width() / 8)
-        ycor = event.y // (self.board.winfo_height() / 8)
-
-        pos = (ycor, xcor)
-        move = _index_to_algebraic(pos)
-        print(move)
-        if move in self.game.get_valid_moves():
-            self.game.make_move(move)
-
-            self.draw_game_state()
-            root.update()
-
-        self.click_pos = pos
-
 
 if __name__ == '__main__':
     root = tk.Tk()
     root.geometry('500x500')
-    gui = ReversiGUI(root, 8)
-    gui.run_game(RandomPlayer(), RandomPlayer())
-    # gui.run_game_human(RandomPlayer(), RandomPlayer())
-    # gui.run_game(RandomPlayer(), GUIPlayer(gui.board, gui.game.get_board_size()))
+    gui = ReversiGUI(root, 8, RandomPlayer(), RandomPlayer())
     root.deiconify()
     root.mainloop()
