c942d08b2bdeb5a21bea07ed37a892d996b846f6
TheXanMan321 <xander4444@icloud.com>
2021-04-15 16:04:40 -0400

    Update simple_tk_gui.py

--- simple_tk_gui.py
+++ simple_tk_gui.py
@@ -11,23 +11,64 @@ Authors:
     - Alexander Nicholas Conway
 This file is Copyright (c) 2021.
 """
-import tkinter as tk
+from typing import Optional
 import time
+
+import tkinter as tk
 
 from mcts import MCTSTimeSavingPlayer
 from reversi import ReversiGame, Player, RandomPlayer, GUIPlayer
 from minimax_tree import MobilityTreePlayer, PositionalTreePlayer
 from constants import BLACK, WHITE, DEFAULT_FPS, index_to_algebraic
-from typing import Optional
 
 
-class ReversiVisualizer:
+class SimpleApp:
+    """A class that represents a simple tkinter application
+
+        Instance Attributes:
+            - root: The tk.Tk instance in the program
+            - frame: The frame of the root
+            - click_wanted: Whether or not the window is waiting for input
+            - click_move: A move corresponding to the mouse click
+            - board: A canvas on which a Reversi board is drawn
+    """
+    root: tk.Tk
+    frame: tk.Frame
+    click_wanted: tk.BooleanVar
+    click_move: str
+    board: tk.Canvas
+
+    def __init__(self, parent: tk.Tk) -> None:
+        """initialize gui
+        """
+        # setting up
+        self.root = parent
+        self.root.title('Reversi')
+        self.frame = tk.Frame(parent)
+        self.click_wanted = tk.BooleanVar()
+        self.click_move = ''
+        self.board = tk.Canvas(self.frame, height=500, width=500, bg='black', bd=-2)
+
+
+class ReversiVisualizer(SimpleApp):
     """
     GUI class that creates a simple, quick visualization of a Reversi Board, without other
     UI elements, like menus, buttons, etc
     This class is responsible for starting a game board, handling game click events, and processing
     and updating current board.
+
+        Instance Attributes:
+            - status_bar: A frame that holds labels displaying game info labels
+            - piece_count_dis: A label showing the number pieces of each colour in Reversi
+            - previous_move_dis:  A label showing the most recent move
+            - current_player_dis: A label showing who's turn it is
+            - game: The game of Reversi being played
     """
+    status_bar: Optional[tk.Frame]
+    piece_count_dis: tk.Label
+    previous_move_dis: tk.Label
+    current_player_dis: tk.Label
+    game: ReversiGame
 
     def __init__(self, parent: tk.Tk, size: int) -> None:
         """initialize gui
@@ -39,9 +80,7 @@ class ReversiVisualizer:
         :param size: size of the game, which is either 8 or 6
         """
         # setting up
-        self.root = parent
-        self.root.title('Reversi')
-        self.frame = tk.Frame(parent)
+        super().__init__(parent)
         self.status_bar = None
 
         self.status_bar = tk.Frame(self.root, bg='blue')
@@ -58,12 +97,10 @@ class ReversiVisualizer:
                                           font=('Helvetica', 16))
         self.previous_move_dis.grid(row=0, column=2)
 
-        self.board = tk.Canvas(self.frame, height=500, width=500, bg='black', bd=-2)
         self.board.pack(pady=10)
         self.frame.pack()
         self.click_wanted = tk.BooleanVar()
         self.board.bind('<Button-1>', self.click)
-        self.click_move = ''
 
         # initialize game
         self.game = ReversiGame(size)
@@ -152,7 +189,7 @@ class ReversiVisualizer:
 
         self.board.pack()
 
-    def click(self, event) -> None:
+    def click(self, event: tk.EventType.Button) -> None:
         """Called when mouse is clicked on the given canvas
         Finds the relative position of the click and executes a move"""
         if self.click_wanted.get():
@@ -206,14 +243,27 @@ class ReversiVisualizer:
         page_1 = GameStartScreen(self.root)
         page_1.main_page()
 
+
 class GameStartScreen:
     """
     starting screen of the game
-    """
 
-    def __init__(self, root=None) -> None:
+    Instance Attributes:
+        - root: The instance of tk.Tk running the application
+        - frame: The tkinter frame the window is using
+        - page_1: The opening screen of the application
+        - player: The AI player playing against the user
+        - boardsize: The size of the board being played on
+    """
+    root: tk.Tk
+    frame: tk.Frame
+    page_1: Optional[ReversiVisualizer]
+    player: Optional[Player]
+    boardsize: Optional[int]
+
+    def __init__(self, tk_root: Optional[tk.Tk] = None) -> None:
         """initializes the main menu of the game"""
-        self.root = root
+        self.root = tk_root
         self.frame = tk.Frame(self.root)
         self.frame.pack()
         player_choices = ['Mobility Player', 'Positional Player', 'Random Player',
@@ -251,7 +301,7 @@ class GameStartScreen:
             self.page_1.start_page()
             self.page_1.run_game(GUIPlayer(), MCTSTimeSavingPlayer(100, 8), 8)
 
-    def set_player(self, value) -> None:
+    def set_player(self, value: str) -> None:
         """drop down menu selecting AI player"""
         if value == 'Mobility Player':
             self.player = MobilityTreePlayer(3)
@@ -262,7 +312,7 @@ class GameStartScreen:
         else:
             self.player = MCTSTimeSavingPlayer(500, 15)
 
-    def set_board(self, value) -> None:
+    def set_board(self, value: str) -> None:
         """drop down menu selecting board size"""
         if value == '8':
             self.boardsize = 8
@@ -276,6 +326,18 @@ class GameStartScreen:
 
 
 if __name__ == '__main__':
-    root = tk.Tk()
-    app = GameStartScreen(root)
-    root.mainloop()
+    # root = tk.Tk()
+    # app = GameStartScreen(root)
+    # root.mainloop()
+    import python_ta
+    import python_ta.contracts
+
+    python_ta.contracts.check_all_contracts()
+    python_ta.check_all(config={
+        'extra-imports': ['tkinter', 'time', 'typing', 'mcts', 'reversi', 'minimax_tree',
+                          'constants'],
+        # the names (strs) of imported modules
+        'allowed-io': ['click', 'run_game'],
+        'max-line-length': 100,
+        'disable': ['E1136']
+    })
